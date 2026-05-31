ec93fcda9ea80b8bda8a7b809c8019b44a95c09c
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-06-01 00:21:33 +0530

    Add Java program to read and print array elements

---
+++ src/nqt_sheet/Input_Array.java
@@ -0,0 +1,22 @@
+package nqt_sheet;
+
+import java.util.Scanner;
+
+public class Input_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Size of Array: ");
+
+        int n = sc.nextInt();
+
+        System.out.println("Enter Element");
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Element of Array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
