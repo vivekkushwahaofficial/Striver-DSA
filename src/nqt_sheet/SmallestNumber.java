31d8a403ccc65f5d7d0151fee8146f9cf5a270f3
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-06-01 02:21:22 +0530

    Add Java program to read array and print the smallest element

---
+++ src/nqt_sheet/SmallestNumber.java
@@ -0,0 +1,47 @@
+package nqt_sheet;
+
+import java.util.Scanner;
+
+public class SmallestNumber {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Size of Array: ");
+//
+//        int n = sc.nextInt();
+//        int[] arr = new int[n];
+//        for(int i = 0; i < n; i++){
+//            arr[i] = sc.nextInt();
+//        }
+//        for (int i = 0; i < n; i++) {
+//        }
+//        Arrays.sort(arr);
+//        System.out.println("Smellest Number: "+ arr[0]);
+//    }
+
+    //Optimal Approach
+    public static int smallestElement(int[] arr, int n) {
+        int min = arr[0];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Size of array: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Enter Element: ");
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int min = smallestElement(arr, n);
+        System.out.println("Smallest number from array: " + min);
+    }
+}
+
+
