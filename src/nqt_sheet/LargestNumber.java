1ad9b871468a68972b849affaad7894f80d39bb5
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-06-02 01:49:57 +0530

    Largest Element in the Array

---
+++ src/nqt_sheet/LargestNumber.java
@@ -0,0 +1,19 @@
+package nqt_sheet;
+
+public class LargestNumber {
+    public static int LargestElement(int[] arr){
+        int max = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 34, 43, 66};
+        int max = LargestElement(arr);
+        System.out.println("Largest Element in the Array: "+ max);
+    }
+}
