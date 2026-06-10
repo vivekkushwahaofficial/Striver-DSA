90c6894de1d19ce4d993b004c0139bd1f4ae8994
vivekkushwahaofficial <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-06-10 07:21:06 +0530

    Calculate sum of the elements of the array

--- src/nqt_sheet_problems_on_arrays/SumOfArray.java
+++ src/nqt_sheet_problems_on_arrays/SumOfArray.java
@@ -1,5 +1,18 @@
 package nqt_sheet_problems_on_arrays;
 
 public class SumOfArray {
-  
+    public static int arraySum(int[] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 5, 3, 8};
+
+        int result = arraySum(arr);
+        System.out.println(result);
+    }
 }
